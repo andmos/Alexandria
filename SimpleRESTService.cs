d2933764df1f257e91b7c9dd54f9770c38deadff
Andreas Mosti <andreas.mosti@gmail.com>
2014-03-06 20:44:28 +0100

    Update SimpleRESTService.cs

--- SimpleRESTService.cs
+++ SimpleRESTService.cs
@@ -12,11 +12,11 @@ namespace Common.Services
 {
     public class SimpleRESTService
     {
-		public async Task<object> GetAsync(string url)
+	public async Task<object> GetAsync(string url)
         {
             var request = (HttpWebRequest)HttpWebRequest.Create(new Uri(url));
-			WebResponse responseObject = await Task.Factory.FromAsync<WebResponse>(request.BeginGetResponse, 
-				request.EndGetResponse, request);
+	    WebResponse responseObject = await Task.Factory.FromAsync<WebResponse>(request.BeginGetResponse, 
+					request.EndGetResponse, request);
             var responseStream = responseObject.GetResponseStream();
             var sr = new StreamReader(responseStream);
             string content = await sr.ReadToEndAsync();
