3252a57b97170131b8cc6e871f9f246347b9119a
Andreas Mosti <andreas.mosti@gmail.com>
2014-03-06 20:25:49 +0100

    Added simple REST - service, retunrs JSON - object.

---
+++ SimpleRESTService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace Common.Services
+{
+    public class SimpleRESTService
+    {
+		public async Task<object> GetAsync(string url)
+        {
+            var request = (HttpWebRequest)HttpWebRequest.Create(new Uri(url));
+			WebResponse responseObject = await Task.Factory.FromAsync<WebResponse>(request.BeginGetResponse, 
+				request.EndGetResponse, request);
+            var responseStream = responseObject.GetResponseStream();
+            var sr = new StreamReader(responseStream);
+            string content = await sr.ReadToEndAsync();
+            
+			return JsonConvert.DeserializeObject(content);
+        }
+
+    }
+}
