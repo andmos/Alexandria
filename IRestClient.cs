5c01a852f8de9d436188e771afb82b2fd88587cb
Andreas Mosti <andreas.mosti@gmail.com>
2014-05-03 01:26:54 +0200

    Edited REST-client, more features.

---
+++ IRestClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+	public interface IRestClient<T> where T : class
+	{
+		Task<T> Get(string id);
+		Task<IList<T>> GetAll();
+	}
+
+
--- SimpleRESTServiceClient.cs
+++ SimpleRESTServiceClient.cs
@@ -6,24 +6,53 @@ using System.Net;
 using System.Text;
 using Newtonsoft.Json;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Collections;
 
-//Requires Newtonsofts JSON.net package from NUGet
-//PCL? Yes we can!
-namespace Common.Services
-{
-    public class SimpleRESTServiceClient
+//Requieres JSON.NET from NuGet 
+
+	public class SimpleRESTServiceClient<T> : IRestClient<T> where T : class
     {
-	public async Task<object> GetAsync(string url)
-        {
-        var request = (HttpWebRequest)HttpWebRequest.Create(new Uri(url));
-	    WebResponse responseObject = await Task.Factory.FromAsync<WebResponse>(request.BeginGetResponse, 
-					request.EndGetResponse, request);
-            var responseStream = responseObject.GetResponseStream();
-            var sr = new StreamReader(responseStream);
-            string content = await sr.ReadToEndAsync();
-            
-			return JsonConvert.DeserializeObject(content);
-        }
+
+		private string m_url; 
+
+		public SimpleRESTServiceClient(string url){
+			m_url = url; 
+		}
+
+
+		public async Task<T> Get(string id = null)
+		{
+			string response = await this.MakeAsyncRequest(id);
+			var result = JsonConvert.DeserializeObject<T>(response);
+			return result;
+		}
+
+		public async Task<IList<T>> GetAll()
+		{
+			var response = await this.MakeAsyncRequest();
+			var result = JsonConvert.DeserializeObject<IList<T>>(response);
+			return result;
+		}
+
+		private Task<string> MakeAsyncRequest(string urlAppend = string.empty)
+		{
+			var request = (HttpWebRequest)WebRequest.Create(m_url + urlAppend);
+			request.ContentType = "application/Json";
+			return Task.Factory.FromAsync(request.BeginGetResponse, asyncResult => request.EndGetResponse(asyncResult), null)
+				.ContinueWith(t => this.ReadFromStreamResponse(t.Result));
+		}
+
+		private string ReadFromStreamResponse(WebResponse response)
+		{
+			using (var responseStream = response.GetResponseStream())
+			{
+				using (var reader = new StreamReader(responseStream))
+				{
+					var content = reader.ReadToEnd();
+					return content;
+				}
+			}
+		}
 
     }
-}
